97f84a815f52af1f9c5302fb9ec54d3e37048cd1
marcosbeltranc <mabc181@hotmail.com>
2025-03-05 18:18:15 -0600

    main

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
